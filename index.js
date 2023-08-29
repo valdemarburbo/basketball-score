8ec0f91bfe6ad7a6136dc247044c2a1590cb5d24
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-08-29 03:12:31 +0100

    Add new game button. Add render function.

--- index.js
+++ index.js
@@ -4,32 +4,44 @@ let guestScore = 0;
 let homeScoreEl = document.getElementById("home-score-el");
 let guestScoreEl = document.getElementById("guest-score-el");
 
+function render() {
+    homeScoreEl.textContent = homeScore;
+    guestScoreEl.textContent = guestScore;
+}
+
 function addOneHome() {
     homeScore += 1;
-    homeScoreEl.textContent = homeScore;
+    render();
 }
 
 function addTwoHome() {
     homeScore += 2;
-    homeScoreEl.textContent = homeScore;
+    render();
 }
 
 function addThreeHome() {
     homeScore += 3;
-    homeScoreEl.textContent = homeScore;
+    render();
 }
 
 function addOneGuest() {
     guestScore += 1;
-    guestScoreEl.textContent = guestScore;
+    render();
+    
 }
 
 function addTwoGuest() {
     guestScore += 2;
-    guestScoreEl.textContent = guestScore;
+    render();
 }
 
 function addThreeGuest() {
     guestScore += 3;
-    guestScoreEl.textContent = guestScore;
+    render();
+}
+
+function reset() {
+    homeScore = 0;
+    guestScore = 0;
+    render();
 }
